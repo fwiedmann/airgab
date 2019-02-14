e0a56e853974a92cb6d8fb3e958ccd710bbb7c1a
fwiedmann <privat@felixwiedmann.de>
2019-02-14 22:21:55 +0100

    update main

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
+	"github.com/fwiedmann/airgab/pkg/metrics"
+
 	"github.com/fwiedmann/airgab/pkg/opts"
 	"github.com/fwiedmann/airgab/pkg/rsync"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	opts := opts.New()
-	r := rsync.InitRsync(opts.User, opts.Host, opts.Source, opts.Destination, opts.Options)
+
+	r := rsync.InitRsync(*opts)
+	r.CheckKey()
 	r.CheckConnection()
-	r.RunSync()
+
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(metrics.AirgabCounter)
+	registry.MustRegister(metrics.AirgabTimeStamp)
+	registry.MustRegister(metrics.AirgabBackupSize)
+
+	go func() {
+		for {
+			r.RunSync()
+
+			metrics.AirgabCounter.Inc()
+			metrics.AirgabTimeStamp.SetToCurrentTime()
+			metrics.AirgabBackupSize.Set(r.GetBackupSize())
+
+			time.Sleep(opts.Interval)
+		}
+	}()
+	http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+	http.ListenAndServe(":9100", nil)
 }
