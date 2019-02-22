3334892d92ac967415c88eb1a94f092f6e556510
Felix Wiedmann <30576206+fwiedmann@users.noreply.github.com>
2019-02-22 11:29:19 +0100

    add: copy ssh-key to users ssh-fodler to prevent issues with ownership

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,13 @@ if [[ -z "$DEBUG" ]]; then
 fi
 
 # check if ssh key is mounted
-if [[ ! -f "/home/pilot/.ssh/id_rsa"  ]];then
+if [[ ! -f "/home/pilot/id_rsa"  ]];then
     echo 'No private ssh-key was found. Please mount the key like this: "/home/pilot/.ssh/id_rsa"'
     exit 1
 fi
+
+# copy ssh-key to users ssh-fodler to prevent issues with ownership
+cp .id_rsa .ssh/id_rsa
 
 # change owner of ssh key to container user
 chown pilot:pilot /home/pilot/.ssh/id_rsa
